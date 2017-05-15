8d5fe0ce3d65ca80fe1f27c1906ed46beff418f8
Kyle Bonar <kylewb2011@gmail.com>
2017-05-15 15:12:00 -0500

    Switch from list of touchable items to flatlist which will allow for scroll-to-refresh.

--- app/Screens/ApiExample.js
+++ app/Screens/ApiExample.js
@@ -1,75 +1,106 @@
 'use strict'
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, ScrollView, Image, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet, FlatList } from 'react-native'
 import axios from 'axios'
 
 import StatusBarBackground from '../Components/StatusBarBackground'
 import GoToHome from '../Components/GoToHome'
-
+import SearchBar from '../Components/SearchBar'
 
 class ApiExample extends Component {
 	constructor(props) {
 		super(props)
 
-		this.state = { feed: undefined }
+		this.state = { 
+			feed: undefined,
+			numResults: 25,
+			nationality: 'us',
+			refresing: false,
+			error: null
+		}
 	}
 
 	componentWillMount() {
 		//invoke api function
-		this.getApi()
+		this._getApi()
 	}
 
 	//function to call arbitrary api and create a view with response data
-	getApi() {
-		axios.get('https://randomuser.me/api/?results=50&nat=us')
+	_getApi() {
+		axios.get('https://randomuser.me/api/?results=' + this.state.numResults + '&nat=' + this.state.nationality)
 			.then( (response) => {
 				this.setState({
-					feed: response.data.results
+					feed: response.data.results,
+					error: response.error || null,
+					loading: false,
+					refreshing: false
 				})
 			})
 			.catch(function (error) {
-				console.log(error);
+				this.setState({
+					error: error,
+					loading: false
+				})
 			});
 	}
 
 
-	createListItems(items) {
-		if(items != undefined) {
-			return (items.map( (item, index) => {
-				if(item.id.value) {
-					return (
-						<TouchableOpacity key={item.id.value} style={styles.personRow}>
-							<Image style={styles.personImage} source={{uri: item.picture.thumbnail}} />
-							<View style={styles.personInformation}>
-								<Text style={styles.personFullName}>{item.name.first} {item.name.last}</Text>
-								<Text style={styles.personEmail}>{item.email}</Text>
-								<Text sytle={styles.personPhone}>{item.phone}</Text>
+	_createListItems({item}) {
+		return (
+			<TouchableOpacity style={styles.personRow}>
+				<Image style={styles.personImage} source={{uri: item.picture.thumbnail}} />
+				<View style={styles.personInformation}>
+					<Text style={styles.personFullName}>
+						{item.name.first} {item.name.last}
+					</Text>
+					<Text style={styles.personEmail}>
+						{item.email}
+					</Text>
+					<Text sytle={styles.personPhone}>
+						{item.phone}
+					</Text>
 
-							</View>
-						</TouchableOpacity>
-					)
-				}
-			}))
-		} else {
-			return <Text style={{textAlign: "center"}}>Loading...</Text>
-		}
+				</View>
+			</TouchableOpacity>
+		)						
+	}
+
+	_keyExtractor(item, index) {
+		return (
+			item.email
+		)
+	}
+
+	_listHeader() {
+		return(
+			<SearchBar  />
+		)
 	}
 
 	render() {
 		return(
-			<View>
+			<View style={{flex:1 }}>
 				<StatusBarBackground></StatusBarBackground>
 				<GoToHome navigator={this.props.navigator} name={this.props.name}></GoToHome>
 				<Text style={{textAlign: "center"}}>Example Directory</Text>
-				<ScrollView>
-					{this.createListItems(this.state.feed)}
-				</ScrollView>
+				<View style={styles.scrollViewContainer}>
+					<FlatList 
+						data={ this.state.feed }
+						renderItem={ this._createListItems }
+						keyExtractor={ this._keyExtractor }
+						ListHeaderComponent={ this._listHeader }
+					/>
+				</View>
 			</View>
 		)
 	}
 }
 
 const styles = StyleSheet.create({
+	scrollViewContainer: {
+		flexDirection: "column",
+		flex: 1,
+	},
 	personRow: {
 		flexDirection: "row",
 		justifyContent: "flex-start",

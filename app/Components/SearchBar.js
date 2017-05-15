14a2ba8c16d9fc8d60b08d6f19be574bdae9b738
Kyle Bonar <kylewb2011@gmail.com>
2017-05-15 15:12:30 -0500

    Initial commit. Search bar from scratch.

---
+++ app/Components/SearchBar.js
@@ -0,0 +1,57 @@
+'use strict'
+import React, { Component } from 'react'
+import { View, TextInput, StyleSheet, Text } from 'react-native'
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			text: "",
+			placeholder: "Search..."
+		}
+	}
+	render() {
+		return(
+			<View style={styles.searchBarContainer}>
+				<View style={styles.searchBarFake} >
+					<TextInput
+						style={styles.searchBar}
+						value={this.state.text}
+						placeholder={this.state.placeholder}
+						onChangeText={(text) => this.setState({text})}
+						/>
+				</View>
+			</View>
+
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	searchBarContainer: {
+		backgroundColor: "#f3f3ff",
+		borderWidth: 0,
+	},
+	searchBarFake: {
+		backgroundColor: "#e8e8ee",
+		flex: 1,
+		borderRadius: 15,
+		marginRight: 10,
+		marginLeft: 10,
+		marginTop: 10,
+		marginBottom: 10,
+		paddingTop: 6,
+		paddingBottom: 6,
+	},
+	searchBar: {	
+		backgroundColor: "#e8e8ee",
+		borderWidth: 0,
+		height: 20,
+		marginLeft: 20,
+		marginRight: 20
+
+	}
+})
+
+module.exports = SearchBar
